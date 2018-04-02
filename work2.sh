8cbfbf1c5ce2a4e983cf39562b427970b3fa6d15
btamrakar <btamrakar03@gmail.com>
2018-04-02 11:00:03 -0400

    added all files.

---
+++ work2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "how are you today?"
+read a
+if [ "$a"  = "OK" ]
+then
+ echo "that is really good"
+else
+echo "everything will be alright.?"
+ fi
+
---
+++ work3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "what is the temperature outside?"
+read a
+
+if [ "$a" -ge 70 ]
+then
+echo "summer is here."
+
+else 
+echo "why is it not summer yet?"
+
+fi
+
